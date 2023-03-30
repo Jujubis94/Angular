390333982e7c09bb307a1fe69b4bee5d30a8cb07
Julien Marcenac <julien.marcenac2@gmail.com>
2023-03-30 12:20:46 +0200

    fix class

--- src/app/secondFilm.directive.ts
+++ src/app/secondFilm.directive.ts
@@ -1,19 +1,18 @@
 import { Directive, HostListener, HostBinding } from '@angular/core';
 
 @Directive({
-  selector: '[appSecondFilm]',
+  selector: '[appSecondFilm]'
 })
 export class SecondFilmDirective {
-  activeClass: boolean = false;
-  
-  @HostBinding('class.changeCss')
 
-  @HostListener('mouseenter')
-  mouseEnter() {
+  @HostBinding('class.changeCss') activeClass: boolean = false;
+
+  @HostListener('mouseenter') mouseEnter() {
     this.activeClass = true;
   }
 
   @HostListener('mouseleave') mouseLeave() {
     this.activeClass = false;
   }
+
 }
